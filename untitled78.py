120b0e0b5b03449d4a622de305bbe5f0823f4034
Moucarriii <114299590+Moucarriii@users.noreply.github.com>
2023-05-29 02:15:45 +0300

    Update untitled78.py

--- untitled78.py
+++ untitled78.py
@@ -1,2 +1,18 @@
 import streamlit as st
-st.title("Plotly Visualizations!!!")
+def main():
+    st.title("My Streamlit App")
+    
+    # Add your Streamlit app content here
+    st.write("Welcome to my app!")
+    
+    # Example of adding a button to the app
+    if st.button("Click me!"):
+        st.write("Button clicked!")
+    
+    # Example of adding a selectbox to the app
+    options = ["Option 1", "Option 2", "Option 3"]
+    selected_option = st.selectbox("Choose an option", options)
+    st.write("You selected:", selected_option)
+    
+if __name__ == "__main__":
+    main()
